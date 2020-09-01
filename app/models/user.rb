166f970ebaefb0b2dfaa48915edacfcbcba4f396
Juan Manuel <44899916+codingAngarita@users.noreply.github.com>
2020-09-01 11:00:49 -0500

    Added validations for email uniqueness & presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,7 @@ class User < ApplicationRecord
 
   # Validations
   validates_presence_of :name
+  validates_presence_of :email
+  validates_uniqueness_of :email
   validates_presence_of :password_digest
 end
