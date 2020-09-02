9ec87e2f9c1b17b481e8e7269b9bf7be91ca4c95
Juan Manuel <44899916+codingAngarita@users.noreply.github.com>
2020-09-02 10:26:59 -0500

    Added helper methods for user authentication

--- spec/support/request_spec_helper.rb
+++ spec/support/request_spec_helper.rb
@@ -1,7 +1,18 @@
 module RequestSpecHelper
   # Parsing json for controller specs
-
   def json
     JSON.parse(response.body)
+  end
+
+  # Generates token given an email & password
+  def login(user_email, user_password)
+    post '/users/login', params: { email: user_email, password: user_password }
+
+    json['token']
+  end
+
+  # Generates headers given a token
+  def token_headers token
+    { 'Authorization': "Bearer #{token}" }
   end
 end
