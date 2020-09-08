dcbc96fcb8769fccf01354de4642f6e11b295045
Juan Manuel <44899916+codingAngarita@users.noreply.github.com>
2020-09-08 06:44:32 -0500

    Simplified user create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,28 +2,20 @@ class UsersController < ApplicationController
   skip_before_action :authorized, only: %i[create login]
 
   def create
+    user_params.require(%i[name email password password_confirmation])
+
     @user = User.create(user_params)
     if @user.valid?
-      token = encode_token({
-                             user_id: @user.id,
-                             user_admin: @user.admin
-                           })
+      token = encode_token({ user_id: @user.id, user_admin: @user.admin})
+
       response_json = { user: UserSerializer.new(@user), token: token }
-      render json: response_json, status: :created
+      response_code = :created
     else
-      missing_params = check_params(
-        user_params,
-        %i[name email password password_confirmation]
-      )
-
-      response_json = if missing_params
-                        { error: "Couldn't create account" }
-                      else
-                        { error: 'Missing fields' }
-                      end
-
-      render json: response_json, status: :bad_request
+      response_json = { error: "Couldn't create account" }
+      response_code = :bad_request
     end
+
+    render json: response_json, status: response_code
   end
 
   def login
